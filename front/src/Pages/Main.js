fc946357837970e0dfe6005d72381f62f9060a03
Nicolas Oyarce V <nicolas.oyarce@mundotelecomunicaciones.cl>
2022-12-30 12:17:41 -0300

    base

---
+++ front/src/Pages/Main.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Container } from "@mui/material";
+
+const Main = () => {
+  return (
+    <Container fixed sx={{ height: 1 }}>
+  Hola. soy la tele
+    </Container>
+  );
+};
+
+export default Main;
